43f2fe0538d9e78f3ad9aa19be203898ea77d019
Nasim-Khalili <khalilinassim85@gmail.com>
2025-07-09 10:45:20 +0330

    Add helper functions for logging OTP and post activities to Elasticsearch

--- accounts/elasticsearch_helpers.py
+++ accounts/elasticsearch_helpers.py
@@ -1,4 +1,5 @@
 from elasticsearch import Elasticsearch
+from django.utils import timezone
 
 es = Elasticsearch("http://localhost:9200")
 
@@ -8,3 +9,15 @@ def save_otp_to_elasticsearch(email, otp_code):
         "otp_code": otp_code
     }
     es.index(index="otp_logs", body=doc)
+    
+
+def log_post_action(user, post, action):
+    doc = {
+        "user_id": user.id if user.is_authenticated else None,
+        "username": user.username if user.is_authenticated else "Anonymous",
+        "post_id": post.id,
+        "post_title": post.title,
+        "action": action,
+        "timestamp": timezone.now().isoformat()
+    }
+    es.index(index="post_logs", body=doc)
